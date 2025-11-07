ee8ea9ccc2cbbd57c3b2dc52c4d375c923851a03
Joshua Green <joshua.kasyoka.green@gmail.com>
2025-11-07 12:41:24 +0000

    changeS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom'
 export default function Header() {
   return (
     <div className="header">
-      <Link to="/" className="brand">Olivia Hingley.</Link>
+      <Link to="/" className="brand">
+        <span>Olivia Hingley.</span>
+      </Link>
       <div className="nav">
         <Link to="/about">About</Link>
       </div>
