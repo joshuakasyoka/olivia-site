de3f2ef28b91e54813876384aad9a48f7a2c462c
Joshua Green <joshua.kasyoka.green@gmail.com>
2025-11-07 11:57:26 +0000

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export default function About() {
   return (
-    <div >
+    <div style={{ backgroundColor: 'red' }}>
       <div className="header">
       </div>
       
